a4c13dda7ea1bad983137be879c9f60821570060
YanaLiz <105737326+YanaLiz@users.noreply.github.com>
2022-10-27 21:06:08 +0300

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,26 +2,30 @@ import Player from "@vimeo/player";
 import throttle from 'lodash.throttle';
 
 
-
 const iframe = document.querySelector("iframe");
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
+
 const STORAGE_KAY = "videoplayer-current-time";
+
+
+
+
+const videoplayer = localStorage.getItem(STORAGE_KAY);
 
 const onPlay = function(data) {
     localStorage.setItem(STORAGE_KAY, data.seconds); 
 };
 
-    
 player.on('timeupdate', onPlay);
 player.getCurrentTime(STORAGE_KAY).then(function(seconds) {
     // seconds = the current playback position
 }).catch(function(error) {
-    
+
 });
 
 player.on("loaded", function () {
-        if (localStorage.getItem(STORAGE_KAY)) {
-            player.setCurrentTime(localStorage.getItem(STORAGE_KAY));
+        if (videoplayer) {
+            player.setCurrentTime(videoplayer);
         }
     });
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,33 +1,38 @@
 import throttle from 'lodash.throttle';
+
+
+
 const STORAGE_KAY = 'feedback-form-state';
+
 const refs = {
     form: document.querySelector('.feedback-form'),
     textarea: document.querySelector('.feedback-form textarea'),
+    email: document.querySelector('.feedback-form input'),
 }
 let formData = {};
+
 savedFormData();
+
 refs.form.addEventListener('submit', throttle(onFormSubmit, 500));
-refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
-refs.form.addEventListener('input', e => {
-    formData[e.target.name] = e.target.value;
+
+refs.form.addEventListener('input', evt => {
+    formData[evt.target.name] = evt.target.value;
     localStorage.setItem(STORAGE_KAY, JSON.stringify(formData));
     console.log(formData);
 });
+
 function onFormSubmit(evt) {
     evt.preventDefault();
     evt.currentTarget.reset();
     localStorage.removeItem(STORAGE_KAY);
 }
-function onTextareaInput(evt) {
-    const message = evt.target.value;
-    localStorage.setItem(STORAGE_KAY, message);
-}
+
 function savedFormData() {
     const savedFormData = localStorage.getItem(STORAGE_KAY);
     const parsedFormData = JSON.parse(savedFormData);
-    if (savedFormData) {
+    if (parsedFormData) {
         formData = parsedFormData;
-        refs.input= formData.email || '';
+        refs.email.value = formData.email || '';
         refs.textarea.value = formData.message || '';
         console.log(formData);
     }
